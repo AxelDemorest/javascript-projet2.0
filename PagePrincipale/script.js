55a33fe08e5d7abba289ab9e7b4fe0f3c8e0ef2d
Cyra <axel.demorest@gmail.com>
2020-11-05 16:40:37 +0100

    JS finition

--- PagePrincipale/script.js
+++ PagePrincipale/script.js
@@ -7,3 +7,76 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+const button = document.getElementById('connectedButton');
+
+button.addEventListener('click', buttonclicked);
+
+function buttonclicked() {
+
+    if(button.textContent === "Se connecter") {
+        document.getElementById('id01').style.display='block';
+    } 
+}
+
+//Accès à la page 1 si l'utilisateur est connecté
+const buttonGame1 = document.getElementById('img1');
+
+buttonGame1.addEventListener('click', buttonclickedGames1);
+
+function buttonclickedGames1() {
+
+    if(button.textContent === "Se connecter") {
+        alert('Tu dois te connecter avant d\'accéder à la page');
+    } else {
+        document.location.href="../deuxieme/page2.html";
+    }
+}
+
+//Accès à la page 2 si l'utilisateur est connecté
+const buttonGame2 = document.getElementById('img2');
+
+buttonGame2.addEventListener('click', buttonclickedGames2);
+
+function buttonclickedGames2() {
+
+    if(button.textContent === "Se connecter") {
+        alert('Tu dois te connecter avant d\'accéder à la page');
+    } else {
+        document.location.href="../troisieme/page3.html";
+    }
+}
+
+//Accès à la page 3 si l'utilisateur est connecté
+const buttonGame3 = document.getElementById('img3');
+
+buttonGame3.addEventListener('click', buttonclickedGames3);
+
+function buttonclickedGames3() {
+
+    if(button.textContent === "Se connecter") {
+        alert('Tu dois te connecter avant d\'accéder à la page');
+    } else {
+        document.location.href="../diepioPage/index.html";
+    }
+}
+
+//bouton pour se connecter
+const formID = document.getElementById('formID');
+
+const inputName = document.getElementById('inputName');
+
+const inputEmail = document.getElementById('inputEmail');
+
+formID.addEventListener('submit', submitForm);
+
+function submitForm() {
+
+    localStorage.setItem('pseudo', inputName.value);
+}
+
+if(button.textContent = "Se connecter" && localStorage.getItem('pseudo')) {
+    button.textContent = "Connecté en tant que : " + localStorage.getItem('pseudo');
+} else {
+    button.textContent = "Se connecter";
+}
